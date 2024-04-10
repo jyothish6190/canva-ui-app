c89adc8a3767e1efc3290e6b464357aa073819ad
Adith-Sukumar-KA <adiths@exalture.com>
2024-04-10 18:48:14 +0530

    feat - Defaultview - EditPage-LivePreview -Themebutton Added

--- src/components/Editpage/EditpageComponents/Livepreview.tsx
+++ src/components/Editpage/EditpageComponents/Livepreview.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Button } from '@canva/app-ui-kit';
 import styles from 'styles/components.css';
+import { DarkThemeIcon, LightThemeIcon } from '../../../assets/image/image.js';
 
 export default function LivePreview() {
     const [bgcolor, setBgColor] = useState<string>('rgba(255, 255, 255, 0.07)');
@@ -12,7 +13,12 @@ export default function LivePreview() {
 
     return (
         <div className={styles.LivePreviewBox} style={{ background: bgcolor }}>
-            <span onClick={HandleOnclick}></span>
+            <span className={styles.ThemeButton} onClick={HandleOnclick}>
+                <img
+                    src={bgcolor == 'white' ? LightThemeIcon : DarkThemeIcon}
+                    alt="themeicon"
+                />
+            </span>
             <Button variant="primary" children="Button" />
         </div>
     );
