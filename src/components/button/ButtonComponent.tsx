90af170e117bd2dd5ba151637812d2c860187b27
Jyothish Jose <jyothish6190@gmail.com>
2024-04-25 09:46:05 +0530

    feat: button component- variant value chnage updated

--- src/components/button/ButtonComponent.tsx
+++ src/components/button/ButtonComponent.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button } from '@canva/app-ui-kit';
 
 import { Component } from 'src/models/component.model';
+import { Variant } from '@canva/app-ui-kit/dist/cjs/ui/apps/developing/ui_kit/components/button/button';
 
 type ButtonComponentType = {
     component: Component;
 };
 
 const ButtonComponent = ({ component }: ButtonComponentType) => {
+    const [variant, setVariant] = useState<Variant>('primary');
+
+    useEffect(() => {
+        component.fields?.forEach((field: Component) => {
+            if (field.name === 'Variant') {
+                setVariant(field.value ? field.value : 'primary');
+            }
+        });
+    }, [component]);
     return (
-        <Button key={component.name} alignment="center" variant="primary">
+        <Button key={component.name} alignment="center" variant={variant}>
             {component.name}
         </Button>
     );
