217afa5ea7890be8f1589747912980a1991d3bab
Adith-Sukumar-KA <adiths@exalture.com>
2024-04-30 18:11:59 +0530

    fet - added live preview of  text input and width input  fields in TypographyCard component

--- src/components/typography-component/TypographyComponent.tsx
+++ src/components/typography-component/TypographyComponent.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { TypographyCard, Text } from '@canva/app-ui-kit';
 
 import ExcessContainer from '../CommonComponents/excessContainer';
 import { Component } from 'src/models/component.model';
+import { TypographyFiledNames } from 'src/constants/component-configs/TypographyConfig';
 
 type PropType = {
     component: Component;
@@ -10,12 +11,58 @@ type PropType = {
     onChange?: (text: any) => void;
 };
 
+type typographyStateData = {
+    cardText: string;
+    cardState: 'default' | 'hover';
+    cardWidth: number;
+};
+
+const initialState: typographyStateData = {
+    cardText: 'The quick brown fox',
+    cardState: 'default',
+    cardWidth: 296,
+};
+
 const TypographyComponent = ({ component, isProperty, onChange }: PropType) => {
+    const [typographyState, setTypographyState] =
+        useState<typographyStateData>(initialState);
+
+    useEffect(() => {
+        component.fields?.forEach((field: Component) => {
+            if (field.name === TypographyFiledNames.TEXT) {
+                setTypographyState((prevState) => {
+                    return {
+                        ...prevState,
+                        cardText: field.value || 'The quick brown fox',
+                    };
+                });
+            }
+            if (field.name === TypographyFiledNames.STATE) {
+                setTypographyState((prevState) => {
+                    return {
+                        ...prevState,
+                        cardState: field.value || 'default',
+                    };
+                });
+            }
+            if (field.name === TypographyFiledNames.WIDTH) {
+                setTypographyState((prevState) => {
+                    return {
+                        ...prevState,
+                        cardWidth: field.value || 296,
+                    };
+                });
+            }
+        });
+    }, [component]);
+
     if (isProperty) {
         return (
-            <TypographyCard onClick={() => {}} ariaLabel="">
-                <Text lineClamp={1}>The quick brown fox</Text>
-            </TypographyCard>
+            <div style={{ width: typographyState.cardWidth + 'px' }}>
+                <TypographyCard onClick={() => {}} ariaLabel="">
+                    <Text lineClamp={1}>{typographyState.cardText}</Text>
+                </TypographyCard>
+            </div>
         );
     }
     return (
