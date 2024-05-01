d45f848e884953604fee61420bb963ceca4aabb0
Adith-Sukumar-KA <adiths@exalture.com>
2024-05-01 11:58:32 +0530

    fet -added live preview for progress bar component

--- src/components/progressbar-component/ProgressbarComponent.tsx
+++ src/components/progressbar-component/ProgressbarComponent.tsx
@@ -1,24 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ProgressBar } from '@canva/app-ui-kit';
 
 import ExcessContainer from '../CommonComponents/excessContainer';
 import { Component } from 'src/models/component.model';
+import { ProgressBarFieldNames } from 'src/constants/component-configs/ProgressbarComponentConfig';
 
 type PropType = {
     component: Component;
     isProperty: boolean;
-    onChange?: (text: any) => void;
 };
 
-const ProgressbarComponent = ({
-    component,
-    isProperty,
-    onChange,
-}: PropType) => {
+type ProgressBarStateData = {
+    progressBarValue: number;
+    progressBarSize: 'small' | 'medium';
+    progressBarWidth: number;
+};
+const initialState: ProgressBarStateData = {
+    progressBarValue: 50,
+    progressBarSize: 'medium',
+    progressBarWidth: 158,
+};
+
+const ProgressbarComponent = ({ component, isProperty }: PropType) => {
+    const [progressBarState, setProgressBarState] =
+        useState<ProgressBarStateData>(initialState);
+
+    useEffect(() => {
+        component.fields?.forEach((field: Component) => {
+            if (field.name === ProgressBarFieldNames.PERCENTAGE) {
+                setProgressBarState((prevState) => {
+                    return {
+                        ...prevState,
+                        progressBarValue: field.value || 50,
+                    };
+                });
+            }
+            if (field.name === ProgressBarFieldNames.SIZE) {
+                setProgressBarState((prevState) => {
+                    return {
+                        ...prevState,
+                        progressBarSize: field.value || 'medium',
+                    };
+                });
+            }
+            if (field.name === ProgressBarFieldNames.WIDTH) {
+                setProgressBarState((prevState) => {
+                    return {
+                        ...prevState,
+                        progressBarWidth: field.value || 158,
+                    };
+                });
+            }
+        });
+    }, [component]);
+
     if (isProperty) {
         return (
-            <div style={{ width: '100%' }}>
-                <ProgressBar size="medium" tone="info" value={50} />
+            <div style={{ width: progressBarState.progressBarWidth }}>
+                <ProgressBar
+                    size={progressBarState.progressBarSize}
+                    tone="info"
+                    value={progressBarState.progressBarValue}
+                />
             </div>
         );
     }
