631a5c7d485090b8c9901009756de856ff9f2344
Adith-Sukumar-KA <adiths@exalture.com>
2024-05-05 17:39:05 +0530

    fet- added live preview for color selection component

--- src/components/color-selector/ColorSelectorComponent.tsx
+++ src/components/color-selector/ColorSelectorComponent.tsx
@@ -1,20 +1,98 @@
-import React from 'react';
-import { ColorSelector, Swatch } from '@canva/app-ui-kit';
+import React, { useEffect, useState } from 'react';
+
+import { ColorSelector, Swatch, Text } from '@canva/app-ui-kit';
+
 import { Component } from 'src/models/component.model';
+import styles from './colorSelector.css';
+import { useComponentStore } from 'src/store/ComponentStore';
+import { ColorFieldNames } from 'src/constants/component-configs/ColorPickerConfig';
 
 type PropType = {
     component: Component;
     isProperty: boolean;
-    onChange?: (text: string) => void;
+};
+type ColorStateData = {
+    color: string;
+    colorlabel: string | undefined;
+    activeState: boolean;
+};
+const initialState: ColorStateData = {
+    color: '#5ba1e7',
+    colorlabel: undefined,
+    activeState: false,
 };
 
-const ColorSelectorComponent = ({
-    component,
-    isProperty,
-    onChange,
-}: PropType) => {
+const ColorSelectorComponent = ({ component, isProperty }: PropType) => {
+    const [colorData, setColorData] = useState<ColorStateData>(initialState);
+    useEffect(() => {
+        component.fields?.forEach((field: Component) => {
+            if (field.name === ColorFieldNames.COLOR_PICKER) {
+                setColorData((prevState) => {
+                    return {
+                        ...prevState,
+                        color: field.value || initialState.color,
+                        colorlabel: '  ',
+                    };
+                });
+            }
+            if (field.name === ColorFieldNames.ACTIVE) {
+                setColorData((prevState) => {
+                    return {
+                        ...prevState,
+                        activeState: field.value || false,
+                    };
+                });
+            }
+        });
+    }, [component]);
+
+    const { selectedComponent, setComponentField, setSelectedComponent } =
+        useComponentStore();
+
+    const changeHandler = (color: string) => {
+        selectedComponent?.fields?.forEach((field: Component) => {
+            if (field.name === component.name) {
+                field.value = color;
+            }
+            setSelectedComponent({ ...selectedComponent });
+            return;
+        });
+        setComponentField(component, color);
+    };
+
     if (isProperty) {
-        return <ColorSelector color="#143F6B" onChange={() => {}} />;
+        return colorData.activeState === false ? (
+            <div className={styles.container}>
+                <Text variant="bold" size="medium">
+                    {colorData.colorlabel
+                        ? colorData.colorlabel
+                        : component.name}
+                </Text>
+                <ColorSelector
+                    color={colorData.color}
+                    onChange={changeHandler}
+                />
+            </div>
+        ) : (
+            <div
+                style={{
+                    position: 'relative',
+                    width: 128,
+                    height: 128,
+                }}
+            >
+                <ColorSelector color={colorData.color} onChange={() => {}} />
+                <img
+                    src={require('assets/images/ColorPicker.png')}
+                    alt="Image 2"
+                    style={{
+                        position: 'absolute',
+                        bottom: -15,
+                        right: -25,
+                    }}
+                />
+            </div>
+        );
     }
 
     return (
--- src/constants/component-configs/ColorPickerConfig.ts
+++ src/constants/component-configs/ColorPickerConfig.ts
@@ -1,15 +1,21 @@
 import { ComponentType } from '../ComponentTypes';
 
+export enum ColorFieldNames {
+    COLOR_PICKER = 'Color picker',
+    ACTIVE = 'Active',
+}
 export const ColorPickerConfig = [
     {
+        showComponent: true,
         type: ComponentType.COLOR_PICKER,
-        name: 'Color picker',
+        name: ColorFieldNames.COLOR_PICKER,
         color: '#E0D9FC',
         categoryId: 'color',
     },
     {
+        showComponent: true,
         type: ComponentType.SWITCH,
-        name: 'Active',
+        name: ColorFieldNames.ACTIVE,
         defaultValue: false,
     },
 ];
