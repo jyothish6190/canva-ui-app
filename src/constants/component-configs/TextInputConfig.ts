31b9a68724bcdcd877c7b6de96c8003807058b50
Adith-Sukumar-KA <adiths@exalture.com>
2024-05-11 19:38:38 +0530

    fix added default value for state

--- src/constants/component-configs/TextInputConfig.ts
+++ src/constants/component-configs/TextInputConfig.ts
@@ -135,7 +135,7 @@ export const TextInputConfig = [
                 value: 'disabled',
             },
         ],
-        value: 'none',
+        value: 'default',
         placeholder: 'Choose a State',
     },
 ];
