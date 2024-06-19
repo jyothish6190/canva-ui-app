ac0392567531b99817fcdcec78e06383b1446619
Adith-Sukumar-KA <adiths@exalture.com>
2024-06-19 11:02:35 +0530

    fix-colorPicker updated to latest figma change

---
+++ src/components/color-selector/ColorSelectorUtils.ts
@@ -0,0 +1,26 @@
+import { elementToSVG, inlineResources } from 'dom-to-svg';
+
+export const ColorPickertoBase64 = async () => {
+    const htmlElement = document.getElementById('colorSvg') as any;
+
+    const svgDocument = elementToSVG(htmlElement);
+
+    console.log('htmlElement', htmlElement);
+    console.log('svgDocumnet', svgDocument);
+
+    // await inlineResources(svgDocument.documentElement);
+
+    const svgString = new XMLSerializer().serializeToString(svgDocument);
+
+    let cleanedSvgString = svgString.replace(
+        /data-stacking-context="true"|<g data-stacking-layer[^/<]*\/>|<!--[^>]*><style\/>|aria-[^"]*"[^"]*"|[^\x20-\x7E]+/g,
+        ''
+    );
+
+    var base64 = btoa(cleanedSvgString);
+
+    var imgSource = `data:image/svg+xml;base64,${base64}`;
+    console.log('Svg', imgSource);
+
+    return imgSource;
+};
