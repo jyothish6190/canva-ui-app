c836a2f19abe5aaf14a2ea3152c0180e49c6d80d
vaisakhcp <vaisakhc@exalture.com>
2024-07-01 12:38:10 +0530

    checkbox selection issue fix

--- src/component-utils/ComponentUtils.ts
+++ src/component-utils/ComponentUtils.ts
@@ -37,3 +37,15 @@ export const getIcon = (iconName: string): Icon | undefined => {
 
     return iconFound;
 };
+
+export const generateUUID = () => {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
+        /[xy]/g,
+        function (c) {
+            const r =
+                (crypto.getRandomValues(new Uint8Array(1))[0] & 0x0f) |
+                (c === 'x' ? 0 : 0x8);
+            return r.toString(16);
+        }
+    );
+};
